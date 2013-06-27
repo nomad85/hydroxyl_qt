7f6cd459b8e3e9eb47cb16e787162bb627e971fa
Admin <nomad13@yandex.ru>
2013-06-27 16:42:47 +0300

    First commit

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     ManageWindow* manageWindow = new ManageWindow();
-//    Hydroxyl*  hydroxyl = new Hydroxyl();
+    Hydroxyl*  hydroxyl = new Hydroxyl();
     InitWindow *initWindow = new InitWindow();
     initWindow->showFullScreen();
     initWindow->setManageWindow(*manageWindow);
